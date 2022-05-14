360f137fc67b248871cc7e3045d96348930973a1
ohmingeon <ohmingeon@gmail.com>
2022-05-14 11:38:52 +0900

    Update scrol.js

--- js/scrol.js
+++ js/scrol.js
@@ -14,23 +14,6 @@ $(document).ready(function(){
             $("#intro2").css({
                 "opacity": -opavalue
             });
-            $("#left-text").css({
-                "letter-spacing" : (-$(window).scrollTop()/(windowheigh*(8/85))),
-                "opacity": opavalue
-            });
-            $("#left-img").css({
-                "opacity": -(opavalue),
-                "cursor": 'pointer'
-            });
-            $(".right-text").css({
-                "transform" : "matrix(1,0,0,1,"+($(window).scrollTop())/(windowheigh/65)+",0)"
-            });
-            $(".scrollmenu").css({
-                "transform" : "matrix(1,0,0,1,70,0)"
-            });
-            $(".about-main").css({
-                "transform" : "matrix(1,0,0,1,0,0)"
-            });
         };
         if (((windowheigh*1.2) <= $(window).scrollTop())&&($(window).scrollTop()<=(windowheigh*1.5))){
             var opavalue = (((windowheigh*1.2)-$(window).scrollTop())/(windowheigh*0.2))+1;
@@ -47,30 +30,8 @@ $(document).ready(function(){
                 'padding-top': '40vh',
                 "width": '100vw'
             });
-            $("#left-text").css({
-                "opacity": '0'
-            });
-            $("#left-img").css({
-                "opacity": '1',
-                'cursor': 'pointer'
-            });
-            $(".right-text").css({
-                "transform" : "matrix(1,0,0,1,70,0)"
-            });
-            $(".scrollmenu").css({
-                "transform" : "matrix(1,0,0,1,"+((((windowheigh*1.2)-$(window).scrollTop())/(windowheigh/215))+70)+",0)"
-            });
-            $(".about-main").css({
-                "transform" : "matrix(1,0,0,1,0,"+(((windowheigh*1.2)-$(window).scrollTop())/(windowheigh/150))+")"
-            });
         };
         if($(window).scrollTop()>=(windowheigh*1.5)){
-            $(".scrollmenu").css({
-                "transform" : "matrix(1,0,0,1,5,0)"
-            });
-            $(".about-main").css({
-                "transform" : "matrix(1,0,0,1,0,-45)"
-            });
             $("#intro2").css({
                 "opacity": '0',
                 "padding-top": '0',
@@ -78,16 +39,6 @@ $(document).ready(function(){
             });
             $(".intro").remove("#intro1");
             $(".intro").remove("#intro2");
-            $(".right-text").css({
-                "transform" : "matrix(1,0,0,1,70,0)"
-            });
-            $("#left-text").css({
-                "opacity": '0'
-            });
-            $("#left-img").css({
-                "opacity": '1',
-                'cursor': 'pointer'
-            });
         };
         if($(window).scrollTop()>=(windowheigh*1.5)){
             
